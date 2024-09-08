282c9587bb81b49041a19ae22206c6a716c1aa6c
jamesbrown1417 <james.brown1417@gmail.com>
2024-09-08 19:29:58 +0930

    automated commit and timestamp 2024-09-08 19:29:58

--- Odds-Scraping/Internationals/master_processing_script.R
+++ Odds-Scraping/Internationals/master_processing_script.R
@@ -15,21 +15,21 @@ run_scraping <- function(script_name) {
 }
 
 # Run all odds scraping scripts-------------------------------------------------
-# run_scraping("Odds-Scraping/CPL/scrape_betr.R")
-# run_scraping("Odds-Scraping/CPL/scrape_BetRight.R")
-# run_scraping("Odds-Scraping/CPL/scrape_pointsbet.R")
-run_scraping("Odds-Scraping/CPL/02-scrape-Sportsbet.R")
-run_scraping("Odds-Scraping/CPL/01-scrape-TAB.R")
-run_scraping("Odds-Scraping/CPL/04-scrape-topsport.R")
-# run_scraping("Odds-Scraping/CPL/Neds/scrape_neds.R")
-# run_scraping("Odds-Scraping/CPL/scrape_dabble.R")
+# run_scraping("Odds-Scraping/Internationals/scrape_betr.R")
+# run_scraping("Odds-Scraping/Internationals/scrape_BetRight.R")
+# run_scraping("Odds-Scraping/Internationals/scrape_pointsbet.R")
+run_scraping("Odds-Scraping/Internationals/02-scrape-Sportsbet.R")
+run_scraping("Odds-Scraping/Internationals/01-scrape-TAB.R")
+run_scraping("Odds-Scraping/Internationals/04-scrape-topsport.R")
+# run_scraping("Odds-Scraping/Internationals/Neds/scrape_neds.R")
+# run_scraping("Odds-Scraping/Internationals/scrape_dabble.R")
 
 #===============================================================================
 # Read in all H2H
 #===============================================================================
 
 # Read in all H2H data
-list_of_h2h_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "h2h")
+list_of_h2h_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "h2h")
 
 # Read in all H2H data
 list_of_h2h_data <-
@@ -62,14 +62,14 @@ best_away_win <-
   rename(away_agency = agency)
 
 # Write out 
-write_rds(h2h_data, "Data/T20s/CPL/processed_odds/h2h.rds")
+write_rds(h2h_data, "Data/T20s/Internationals/processed_odds/h2h.rds")
 
 #===============================================================================
 # Runs at fall of first wicket
 #===============================================================================
 
 # Read in all FOFW data
-list_of_fofw_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "runs_at_first_wicket.csv")
+list_of_fofw_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "runs_at_first_wicket.csv")
 
 # Read in all FOFW data
 list_of_fofw_data <- map(list_of_fofw_files, read_csv)
@@ -83,14 +83,14 @@ fofw_data <-
   select(-home_team, -away_team)
 
 # Write out 
-write_rds(fofw_data, "Data/T20s/CPL/processed_odds/runs_at_first_wicket.rds")
+write_rds(fofw_data, "Data/T20s/Internationals/processed_odds/runs_at_first_wicket.rds")
 
 #===============================================================================
 # First Over Runs
 #===============================================================================
 
 # Read in all first over runs data
-list_of_for_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "first_over_runs.csv")
+list_of_for_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "first_over_runs.csv")
 
 # Read in all first over runs data
 list_of_for_data <- map(list_of_for_files, read_csv)
@@ -103,14 +103,14 @@ for_data <-
   arrange(match, team)
 
 # Write out 
-write_rds(for_data, "Data/T20s/CPL/processed_odds/first_over_runs.rds")
+write_rds(for_data, "Data/T20s/Internationals/processed_odds/first_over_runs.rds")
 
 #===============================================================================
 # Match Sixes
 #===============================================================================
 
 # Read in all match sixes data
-list_of_ms_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "match_sixes")
+list_of_ms_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "match_sixes")
 
 # Read in all match sixes data
 list_of_ms_data <- map(list_of_ms_files, read_csv)
@@ -124,14 +124,14 @@ ms_data <-
   select(-home_team, -away_team)
 
 # Write out 
-write_rds(ms_data, "Data/T20s/CPL/processed_odds/match_sixes.rds")
+write_rds(ms_data, "Data/T20s/Internationals/processed_odds/match_sixes.rds")
 
 #===============================================================================
 # Match Fours
 #===============================================================================
 
 # Read in all match fours data
-list_of_mf_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "match_fours")
+list_of_mf_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "match_fours")
 
 # Read in all match fours data
 list_of_mf_data <- map(list_of_mf_files, read_csv)
@@ -145,14 +145,14 @@ mf_data <-
   select(-home_team, -away_team)
 
 # Write out 
-write_rds(mf_data, "Data/T20s/CPL/processed_odds/match_fours.rds")
+write_rds(mf_data, "Data/T20s/Internationals/processed_odds/match_fours.rds")
 
 #===============================================================================
 # Team Sixes
 #===============================================================================
 
 # Read in all team sixes data
-list_of_ts_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "team_sixes")
+list_of_ts_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "team_sixes")
 
 # Read in all team sixes data
 list_of_ts_data <- map(list_of_ts_files, read_csv)
@@ -166,14 +166,14 @@ ts_data <-
   select(-home_team, -away_team)
 
 # Write out
-write_rds(ts_data, "Data/T20s/CPL/processed_odds/team_sixes.rds")
+write_rds(ts_data, "Data/T20s/Internationals/processed_odds/team_sixes.rds")
 
 #===============================================================================
 # Team Fours
 #===============================================================================
 
 # Read in all team fours data
-list_of_tf_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "team_fours")
+list_of_tf_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "team_fours")
 
 # Read in all team fours data
 list_of_tf_data <- map(list_of_tf_files, read_csv)
@@ -187,14 +187,14 @@ tf_data <-
   select(-home_team, -away_team)
 
 # Write out
-write_rds(tf_data, "Data/T20s/CPL/processed_odds/team_fours.rds")
+write_rds(tf_data, "Data/T20s/Internationals/processed_odds/team_fours.rds")
 
 #===============================================================================
 # Batter Runs
 #===============================================================================
 
 # Read in all batter runs data
-list_of_br_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "player_runs")
+list_of_br_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "player_runs")
 
 # Read in all batter runs data
 list_of_br_data <-
@@ -210,14 +210,14 @@ br_data <-
   arrange(match,player_name, line, desc(over_price))
 
 # Write out
-write_rds(br_data, "Data/T20s/CPL/processed_odds/player_runs.rds")
+write_rds(br_data, "Data/T20s/Internationals/processed_odds/player_runs.rds")
 
 #===============================================================================
 # Bowler Wickets
 #===============================================================================
 
 # Read in all bowler wickets data
-list_of_bw_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "player_wickets")
+list_of_bw_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "player_wickets")
 
 # Read in all bowler wickets data
 list_of_bw_data <- map(list_of_bw_files, read_csv)
@@ -232,14 +232,14 @@ bw_data <-
   arrange(match,player_name, line, desc(over_price))
 
 # Write out
-write_rds(bw_data, "Data/T20s/CPL/processed_odds/player_wickets.rds")
+write_rds(bw_data, "Data/T20s/Internationals/processed_odds/player_wickets.rds")
 
 #===============================================================================
 # Top Team Wicket Taker
 #===============================================================================
 
 # Read in all top team wicket taker data
-list_of_ttwt_files <- list.files("Data/T20s/CPL/scraped_odds/", full.names = TRUE, pattern = "top_team_wicket_taker")
+list_of_ttwt_files <- list.files("Data/T20s/Internationals/scraped_odds/", full.names = TRUE, pattern = "top_team_wicket_taker")
 
 # Read in all top team wicket taker data
 list_of_ttwt_data <- map(list_of_ttwt_files, read_csv)
